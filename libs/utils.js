6a5175c10a0dc22c746e398cf0d37be70570297f
Khalifa S <ibrahim.idris.developer@gmail.com>
2021-08-22 13:49:08 +0100

    Modify utils.js to use process.env

--- libs/utils.js
+++ libs/utils.js
@@ -1,6 +1,9 @@
 const randomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-const debugMode = () => process.argv.slice(-1)[0] === "-d";
+const debugMode = () => {
+    if (process.argv) return process.argv.slice(-1)[0] === "-d";
+    return false;
+};
 
 module.exports = {
     randomInteger,
